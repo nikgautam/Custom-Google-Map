4bfc03d57ecc28b7b4b39378625de9f9da67460a
Nikhil <37222538+nikgautam@users.noreply.github.com>
2021-12-10 21:51:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapId : '270f9e127fa42949'
+  });
+}
